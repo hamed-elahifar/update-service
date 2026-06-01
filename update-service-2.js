1ac725b34884b59a2d4d3bd8219bb705b2544862
Hamed ElahiFar <hamed.elahifar@gmail.com>
2026-06-01 17:10:49 +0330

    improve update-service-2

--- update-service-2.js
+++ update-service-2.js
@@ -7,44 +7,41 @@ const config = require("./config.json");
 
 const { apps, port, password } = config;
 
-const executeCommand = (command) => {
+const streamCommand = (command, res) => {
   return new Promise((resolve, reject) => {
     console.log(`>>> ${command}`);
 
     const result = spawn(command, { shell: true });
 
-    let allOutput = "";
-
     result.stdout.on("data", (data) => {
-      process.stdout.write(data + "\n");
-      allOutput += data + "\n";
+      process.stdout.write(data);
+      res.write(data);
     });
 
     result.stderr.on("data", (data) => {
-      process.stderr.write(data + "\n");
-      allOutput += data + "\n";
+      process.stderr.write(data);
+      res.write(data);
     });
 
     result.on("close", (code) => {
       if (code === 0) {
-        resolve(allOutput);
+        resolve();
       } else {
-        reject(allOutput + `\nCommand exited with code ${code}`);
+        reject(`\nCommand exited with code ${code}`);
       }
     });
   });
 };
 
-const runDeploy = {};
 const appsName = [];
+const runDeploy = {};
 
 apps.forEach((app) => {
-  runDeploy[app.name] = () => {
-    const deployScript = `${app.path}/bin/deploy.sh`;
-    return executeCommand(`bash "${deployScript}"`);
-  };
-
   appsName.push(app.name);
+  runDeploy[app.name] = (res) => {
+    const deployScript = `${app.path}/bin/deploy.sh`;
+    return streamCommand(`bash "${deployScript}"`, res);
+  };
 });
 
 const server = http.createServer(async (req, res) => {
@@ -56,30 +53,29 @@ const server = http.createServer(async (req, res) => {
   console.log("IP:", req.socket.remoteAddress);
   console.log("Method:", req.method);
 
-  const send = (text) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(text);
-  };
-
   if (pass != password) {
-    send("Unauthorized!");
+    res.writeHead(401, { "Content-Type": "text/plain" });
+    res.end("Unauthorized!");
     return;
   }
 
   if (!appsName.includes(appName)) {
-    send("Wrong app name");
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Wrong app name");
     return;
   }
 
-  let allOutput = "";
+  res.writeHead(200, {
+    "Content-Type": "text/plain; charset=utf-8",
+    "Transfer-Encoding": "chunked",
+    "X-Content-Type-Options": "nosniff",
+  });
 
   try {
-    const deployResult = await runDeploy[appName]();
-    allOutput += deployResult + "\n\n";
+    await runDeploy[appName](res);
+    res.end("\n\nDone.\n");
   } catch (error) {
-    allOutput += "Error during deploy:\n" + error + "\n\n";
-  } finally {
-    send(allOutput);
+    res.end("\n\nError during deploy:\n" + error + "\n");
   }
 });
 
